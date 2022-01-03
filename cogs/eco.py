614cb5783ceca60aeee1a9936a1e0c67b0ba0154
koshyj8 <koshyj88@gmail.com>
2022-01-03 15:34:23 +0400

    fixed a couple errors

--- cogs/eco.py
+++ cogs/eco.py
@@ -1,24 +1,93 @@
 import discord
 from discord.ext import commands
+import asyncio
 import sqlite3
 import time
 import datetime
 import random
 from discord.ext.commands.converter import MemberConverter
+from discord.ext import menus
+import json
 
+class LeaderboardMenu(menus.ListPageSource):
+	def __init__(self, data):
+		super().__init__(data, per_page=5)
+
+	async def format_page(self, menu, entries):
+		embed = discord.Embed(
+			title='🏆 Top 10 users 🏆', description='Sorted by the total of wallet and bank balance combined', color = discord.Color.random())
+		place = 0
+
+		for row in entries:
+			place += 1
+			user = row[0]
+			embed.add_field(
+				name=f'{user}', value=f"**Bank:** {row[1]}\n**Wallet:** {row[2]}\n**Total:** {row[1] + row[2]}", inline=False)
+		return embed
+
+class InventoryMenu(menus.ListPageSource):
+	def __init__(self, data, user):
+		self.user = user
+		super().__init__(data, per_page=6)
+
+	async def format_page(self, menu, entries):
+		embed = discord.Embed(title=f"{self.user.name}'s Inventory", color = discord.Color.random())
+
+		for row in entries:
+			if row[2] and row[2] != 0:
+				embed.add_field(
+					name=row[1], value=f"**Amount:** {row[2]}\n**ID:** {row[0]}")
+		return embed
+
+from core.utils.pagin import paginator, quickpaginator
+import aiosqlite
 db = sqlite3.connect("database\eco.sqlite")
-
+await_db = sqlite3.connect("database\eco.sqlite")
 class Economy(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 	
+	@commands.command(aliases=['lb'])
+	async def leaderboard(self,ctx):
+		async with aiosqlite.connect('database\eco.sqlite') as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute('SELECT * FROM user')
+				rows = await cursor.fetchall()
+				await connection.commit()
+
+		top = sorted(rows,reverse=True, key=lambda x: x[1] + x[2])[:10]
+		top = [list(x) for x in top]
+		place = 0
+
+		for row in top:
+			if place == 0:
+				pl = f"🥇 "
+			elif place == 1:
+				pl = f"🥈 "
+			elif place == 2:
+				pl = f"🥉 "
+			else:
+				pl = ''
+			top[place][0] = f'{pl}{place+1}. {await self.bot.fetch_user(row[0])}' or "User not found"
+			place += 1
+
+		pages = menus.MenuPages(source=LeaderboardMenu(top), delete_message_after=True)
+		await pages.start(ctx)
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+		# Use the below line to debug if the cog stops working
+		#print('Economy Cog is active on startup')
+		async with aiosqlite.connect("database\eco.sqlite") as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute("CREATE TABLE IF NOT EXISTS shop (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price INTEGER, available BOOL);")
+				await connection.commit()
+
 	@commands.command()
 	async def create(self, ctx):
 		cursor = db.cursor()
 		cursor.execute(f'SELECT member_id from user WHERE member_id = {ctx.author.id}') 
 		result = cursor.fetchone()
-		date_time = datetime.datetime.now()
-
 
 		if result is None:
 			sql = ('INSERT INTO user(member_id, bank, wallet) VALUES(?,?,?)')
@@ -278,7 +347,7 @@ class Economy(commands.Cog):
 		pattern = '%Y.%m.%d %H:%M:%S'
 		time_now = datetime.datetime.now()
 		epoch = int(time.mktime(time.strptime(
-                    str(time_now).partition('.')[0].replace('-', '.'), pattern)))
+					str(time_now).partition('.')[0].replace('-', '.'), pattern)))
 
 		cursor = db.cursor()
 		cursor.execute(f"SELECT member_id, wallet, last_rob FROM user WHERE member_id = {ctx.author.id}")
@@ -420,6 +489,121 @@ class Economy(commands.Cog):
 				await ctx.send(f"`{user_to_rob.name} has been robbed in the last 12 hours. Please give them a break.`")
 
 	#TODO: Add inventory, shop, buy, sell, gambling etc.
+	
+	@commands.group(invoke_without_command=True)
+	async def shop(self, ctx):
+		async with aiosqlite.connect("database\eco.sqlite") as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute("SELECT * FROM shop WHERE available = ?",(True,))
+				rows = await cursor.fetchall()
+		embed = discord.Embed(title="Shop",description="Use the buy command to buy an item by its id", color = discord.Color.random())
+		for row in rows:
+			embed.add_field(name=row[1],value=f"**Cost**: {row[2]} \n**ID**: {row[0]}")
+		await ctx.send(embed=embed)
+
+	@shop.command()
+	@commands.is_owner()
+	async def edit(self,ctx,item_id,price):
+		item_id = int(item_id)
+		price = int(price)
+		async with aiosqlite.connect("database\eco.sqlite") as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute("UPDATE shop SET price = ? WHERE id = ?",(price,item_id,))
+				await connection.commit()
+		await ctx.send(f"Successfully changed price of item `{item_id}` to `{price}`")
+
+	@shop.command()
+	@commands.is_owner()
+	async def enable(self,ctx,item_id):
+		item_id = int(item_id)
+		async with aiosqlite.connect("database\eco.sqlite") as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute("SELECT * FROM shop WHERE id = ? AND available = ?",(item_id,False,))
+				rows = await cursor.fetchone()
+				if rows:
+					await ctx.send(f"Successfully enabled item `{item_id}` in the shop")
+				else:
+					await ctx.send("That item doesnt exist or is enabled")
+				await cursor.execute("UPDATE shop SET available = ? WHERE id = ?",(True,item_id,))
+				await connection.commit()
+
+	@shop.command()
+	@commands.is_owner()
+	async def remove(self,ctx,item_id):
+		item_id = int(item_id)
+		async with aiosqlite.connect("database\eco.sqlite") as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute("SELECT * FROM shop WHERE id = ? AND available = ?",(item_id,True,))
+				rows = await cursor.fetchone()
+				if rows:
+					await ctx.send(f"Successfully removed item `{item_id}` from the shop")
+				else:
+					await ctx.send("That item doesnt exist")
+				await cursor.execute("UPDATE shop SET available = ? WHERE id = ?",(False,item_id,))
+				await connection.commit()
+
+	@shop.command()
+	@commands.is_owner()
+	async def add(self,ctx,name,price):
+		price = int(price)
+		async with aiosqlite.connect("database\eco.sqlite") as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute("INSERT INTO shop (name, price, available) VALUES (?,?,?)",(name,price,True,))
+				await cursor.execute('SELECT id FROM shop')
+				rows = await cursor.fetchall()
+				number = rows[-1][0]
+				await cursor.execute(f'ALTER TABLE user ADD COLUMN item{number} INTEGER;')
+				await connection.commit()
+		await ctx.send(f"Successfully added `{name}` to the shop at the price of `{price}`")
+
+	@shop.command()
+	async def buy(self,ctx,item_id,amount = 1):
+		item_id,amount = int(item_id),int(amount)
+		async with aiosqlite.connect("database\eco.sqlite") as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute("SELECT name, price FROM shop WHERE id = ? AND available = ?",(item_id,True,))
+				row = await cursor.fetchone()
+				await cursor.execute(f'SELECT item{item_id} FROM user WHERE member_id = {ctx.author.id}')
+				row1 = await cursor.fetchone()
+				await connection.commit()
+		price,name = row[1] * amount,row[0]
+		async with aiosqlite.connect("database\eco.sqlite") as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute("SELECT wallet FROM user WHERE member_id = ?",(ctx.author.id,))
+				row = await cursor.fetchone()
+				if row[0] >= price:
+					if row1[0]:
+						await cursor.execute(f"UPDATE user SET wallet = ?,item{item_id} = ? WHERE member_id = ?",(row[0] - price,row1[0] + amount, ctx.author.id,))
+						await connection.commit()
+						await ctx.send(f"`Successfully purchased {amount} {name}{'s' if amount > 1 else ''} for {price} coins.`")
+					else:
+						await cursor.execute(f"UPDATE user SET wallet = ?,item{item_id} = ? WHERE member_id = ?",(row[0] - price,amount, ctx.author.id,))
+						await connection.commit()
+						await ctx.send(f"`Successfully purchased {amount} {name}{'s' if amount > 1 else ''} for {price} coins.`")
+				else:
+					await ctx.send("`You don't have enough coins.`")
+
+	@commands.command(aliases=['inv'])
+	async def inventory(self,ctx,user:MemberConverter = None):
+		user = user or ctx.author
+		async with aiosqlite.connect('database\eco.sqlite') as connection:
+			async with connection.cursor() as cursor:
+				await cursor.execute('SELECT id,name FROM shop')
+				rows1 = await cursor.fetchall()
+				amount = rows1[-1][0]
+				rows = ','.join([f'item{x + 1}' for x in range(amount)])
+				await cursor.execute(f'SELECT {rows} FROM user WHERE member_id = ?',(user.id,))
+				row = await cursor.fetchone()
+				await connection.commit()
+			new_row = []
+			for item in rows1:
+				new = list(item)
+				new.append(row[item[0] - 1])
+				new_row.append(tuple(new))
+			pages = menus.MenuPages(source=InventoryMenu(new_row,user), delete_message_after=True)
+			await pages.start(ctx)
+
+
 
 def setup(bot):
 	bot.add_cog(Economy(bot))
